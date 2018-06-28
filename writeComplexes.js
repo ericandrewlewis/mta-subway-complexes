1e8240c588690b5144fdbb93ce3d73bd9ab97999
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-27 21:12:11 -0400

    Add borough to complex

--- writeComplexes.js
+++ writeComplexes.js
@@ -8,6 +8,7 @@ const fetchStations = () => {
     .then(text => {
       return new Promise((resolve, reject) => {
         parse(text, {columns: true}, function(err, stations) {
+          if (err) throw err;
           resolve(stations);
         });
       });
@@ -19,11 +20,47 @@ const fetchComplexes = () => {
     .then(response => response.text())
     .then(text => {
       return new Promise((resolve, reject) => {
-        parse(text, {columns: true}, function(err, stations) {
-          resolve(stations);
+        parse(text, {columns: true}, function(err, complexes) {
+          if (err) throw err;
+          resolve(complexes);
         });
       });
     });
+}
+
+const extraComplexBoroughs = {
+  601: "M",
+  602: "M",
+  603: "Bx",
+  604: "Bx",
+  605: "M",
+  606: "Q",
+  607: "M",
+  608: "Bk",
+  609: "M",
+  610: "M",
+  611: "M",
+  612: "M",
+  613: "M",
+  614: "M",
+  615: "Bk",
+  616: "Q",
+  617: "Bk",
+  618: "M",
+  619: "M",
+  620: "Bk",
+  621: "Bk",
+  622: "M",
+  623: "M",
+  624: "M",
+  625: "M",
+  626: "Bk",
+  627: "Bk",
+  628: "Bk",
+  629: "Bk",
+  630: "Bk",
+  635: "M",
+  636: "Bk",
 }
 
 const buildJSON = () => {
@@ -40,7 +77,8 @@ const buildJSON = () => {
       json[id] = {
         id,
         name,
-        daytimeRoutes: []
+        daytimeRoutes: [],
+        borough: extraComplexBoroughs[id]
       };
     });
     stations.forEach(station => {
@@ -50,12 +88,13 @@ const buildJSON = () => {
       if (json[id]) {
         complex = json[id];
       } else {
-        complex = { 
+        complex = {
           id,
           name,
           daytimeRoutes: []
         };
       }
+      complex.borough = station['Borough'];
       complex.latitude = station['GTFS Latitude'];
       complex.longitude = station['GTFS Longitude'];
       complex.daytimeRoutes = complex.daytimeRoutes.concat(station['Daytime Routes'].split(' '));
